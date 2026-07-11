106c4e47e016fae69a5f44be077f3b0be44facd0
Anshika Garg <147253653+anshikagarga@users.noreply.github.com>
2026-07-11 17:56:35 +0530

    Change Something

---
+++ 01-Basics/Class and Object/Demo.java
@@ -0,0 +1,15 @@
+// package 01-Basics.Class and Object;
+
+class Calculator{
+    public int add(int a, int b){
+        return a+b;
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5, 3));
+    }
+    
+}
